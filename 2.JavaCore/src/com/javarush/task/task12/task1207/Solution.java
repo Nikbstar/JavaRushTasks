0ae9feddae5f65b5953771caf72515e82ac0c956
Nikolay Kotkin <nikbstar@gmail.com>
2018-02-04 21:23:18 +0500

    Java Core, 2 уровень, 11 лекция

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(9);
+        print(new Integer(9));
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int age) {
+
+    }
+    public static void print(Integer num) {
+
+    }
+}
