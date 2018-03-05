10c17048b3ca0a427cd7ecb6f3acfd20c1d3886a
Nikolay Kotkin <nikbstar@gmail.com>
2018-03-05 23:56:12 +0500

    Java Core, 10 уровень, 1 лекция

--- 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -21,12 +21,57 @@ public class Solution {
         StringBuilder sb = new StringBuilder();
         reader.close();
         while (fr.ready()) {
-            sb.append(fr.readLine()).append(" ");
+            sb.append(fr.readLine()).append("");
         }
         fr.close();
         List<String> strings = new ArrayList<>();
-        String firstTag = String.format("<%s", args[0]);
-        String lastTag = String.format("</%s>", args[0]);
-
+        String str = sb.toString();
+        str = str.replaceAll(" +", " ");
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '<') {
+                try {
+                    strings.add(findTag(str.substring(i), args[0]));
+                } catch (WrongTagException wte) {
+                }
+            }
+        }
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+    private static String findTag(String str, String tag) throws WrongTagException {
+        StringBuilder sb = new StringBuilder();
+        String firstTag = String.format("<%s", tag);
+        String lastTag = String.format("</%s>", tag);
+        sb.append(firstTag);
+        if (!str.startsWith(firstTag)) {
+            throw new WrongTagException();
+        }
+        for (int i = 0 + firstTag.length(); i < str.length(); i++) {
+            if (str.charAt(i) == '<') {
+                if (endTag(str.substring(i), lastTag)) {
+                    sb.append(lastTag);
+                    break;
+                }
+                try {
+                    String tmp = findTag(str.substring(i), tag);
+                    sb.append(tmp);
+                    i += tmp.length() - 1;
+                } catch (WrongTagException wte) {
+                    sb.append(str.charAt(i));
+                }
+            }
+            else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+    private static boolean endTag(String str, String lastTag) {
+        boolean result = false;
+        if (str.startsWith(lastTag)) {
+            result = true;
+        }
+        return result;
     }
 }
---
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/WrongTagException.java
@@ -0,0 +1,4 @@
+package com.javarush.task.task19.task1918;
+
+public class WrongTagException extends Exception {
+}
