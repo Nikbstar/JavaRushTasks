9ad21c1d525c158f496fb13cd7008a24d57c1e3d
Nikbstar <Nikbstar@gmail.com>
2018-03-20 15:59:11 +0500

    Java Core, 10 уровень, 10 лекция (Кроссворд) закончил

--- 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task20.task2027;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /* 
@@ -14,7 +15,9 @@ public class Solution {
                 {'m', 'l', 'p', 'r', 'r', 'h'},
                 {'p', 'o', 'e', 'e', 'j', 'j'}
         };
-        detectAllWords(crossword, "home", "same");
+        for (Word word : detectAllWords(crossword, "u", "home", "same")) {
+            System.out.println(word);
+        }
         /*
 Ожидаемый результат
 home - (5, 3) - (2, 0)
@@ -23,30 +26,77 @@ same - (1, 1) - (4, 1)
     }
 
     public static List<Word> detectAllWords(int[][] crossword, String... words) {
+        List<Word> wordsResult = new ArrayList<>();
         for (String word : words) {
-            int startX;
-            int endX;
-            int startY;
-            int endY;
             for (int i = 0; i < crossword.length; i++) {
                 for (int j = 0; j < crossword[i].length; j++) {
-                    if (crossword[i][j] == word.charAt(0)) {
-                        startX = i;
-                        startY = j;
-                        int[] dir = checkDirection(crossword, word.charAt(1), startX, startY);
-                    } else if (crossword[i][j] == word.charAt(word.length() - 1)) {
-                        endX = i;
-                        endY = j;
+                    if (word.length() == 1 && crossword[i][j] == word.charAt(0)) {
+                        Word tmp = new Word(word);
+                        tmp.setStartPoint(j, i);
+                        tmp.setEndPoint(j, i);
+                        wordsResult.add(tmp);
+                    } else {
+                        if (crossword[i][j] == word.charAt(0)) {
+                            int[] endPoint = checkWord(crossword, word, i, j);
+                            if (endPoint[0] != 0 || endPoint[1] != 0) {
+                                Word tmp = new Word(word);
+                                tmp.setStartPoint(j, i);
+                                tmp.setEndPoint(endPoint[1], endPoint[0]);
+                                wordsResult.add(tmp);
+                            }
+                        }
+                        if (crossword[i][j] == word.charAt(word.length() - 1)) {
+                            int[] startPoint = checkWord(crossword, reverse(word), i, j);
+                            if (startPoint[0] != 0 || startPoint[1] != 0) {
+                                Word tmp = new Word(word);
+                                tmp.setStartPoint(startPoint[1], startPoint[0]);
+                                tmp.setEndPoint(j, i);
+                                wordsResult.add(tmp);
+                            }
+                        }
                     }
                 }
             }
         }
-        return null;
+        return wordsResult;
     }
-    private static int[] checkDirection(int[][] crossword, char nextChar, int i, int j) {
-        if (i + 1 < crossword.length) {
-
+    private static String reverse(String word) {
+        return new StringBuilder(word).reverse().toString();
+    }
+    private static int[] checkWord(int[][] crossword, String word, int startI, int startJ) {
+        int[] dir = new int[2];
+        int[] result = {0, 0};
+        char nextChar = word.charAt(1);
+        if (startI + word.length() <= crossword.length && (char) crossword[startI + 1][startJ] == nextChar) {
+            dir[0] = 1;
+            dir[1] = 0;
+        } else if (startJ + word.length() <= crossword[startI].length && (char) crossword[startI][startJ + 1] == nextChar) {
+            dir[0] = 0;
+            dir[1] = 1;
+        } else if (startI + word.length() <= crossword.length && startJ + word.length() <= crossword[startI].length && (char) crossword[startI + 1][startJ + 1] == nextChar) {
+            dir[0] = 1;
+            dir[1] = 1;
+        } else {
+            dir[0] = -1;
         }
+        if (dir[0] != -1) {
+            boolean checkWord = true;
+            int endI = startI;
+            int endJ = startJ;
+            for (int i = 1; i < word.length(); i++) {
+                endI += dir[0];
+                endJ += dir[1];
+                if (word.charAt(i) != crossword[endI][endJ]) {
+                    checkWord = false;
+                    break;
+                }
+            }
+            if (checkWord) {
+                result[0] = endI;
+                result[1] = endJ;
+            }
+        }
+        return result;
     }
 
     public static class Word {
