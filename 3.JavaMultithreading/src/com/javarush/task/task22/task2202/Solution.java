d62fb4d3f67060cc046dd0fd874aabc4d4a8a066
Nikolay Kotkin <nikbstar@gmail.com>
2018-03-25 16:06:40 +0500

    Java Multithreading, 2 уровень, 7 лекция

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
@@ -5,32 +5,32 @@ package com.javarush.task.task22.task2202;
 */
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(getPartOfString("JavaRush"));
+        System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java."));
     }
 
     public static String getPartOfString(String string) {
-        int firstSpace = string.indexOf(' ') + 1;
-        int lastSpace = firstSpace;
-        for (int i = 0; i < 4; i++) {
-            if (lastSpace == -1) {
-                throw new TooShortStringException();
-            }
-            lastSpace = string.indexOf(' ', lastSpace + 1);
+
+        try {
+            String[] arr = string.split(" ");
+            return String.format("%s %s %s %s", arr[1], arr[2], arr[3], arr[4]);
+        } catch (Exception re) {
+            throw new TooShortStringException();
         }
-        if (lastSpace == -1) {
-            lastSpace = string.length();
-        }
-        return string.substring(firstSpace, lastSpace);
+
+//        int firstSpace = string.indexOf(' ') + 1;
+//        int lastSpace = firstSpace;
+//        for (int i = 0; i < 4; i++) {
+//            if (lastSpace == -1) {
+//                throw new TooShortStringException();
+//            }
+//            lastSpace = string.indexOf(' ', lastSpace + 1);
+//        }
+//        if (lastSpace == -1) {
+//            lastSpace = string.length();
+//        }
+//        return string.substring(firstSpace, lastSpace);
     }
 
     public static class TooShortStringException extends RuntimeException {
-
-        public TooShortStringException() {
-            super();
-        }
-
-        public TooShortStringException(String m) {
-            super(m);
-        }
     }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task22.task2203;
+
+/* 
+Между табуляциями
+*/
+public class Solution {
+    public static String getPartOfString(String string) throws TooShortStringException {
+        try {
+            String[] arr = string.split("\t");
+            if (arr.length < 3) throw new Exception();
+            return arr[1];
+        } catch (Exception e) {
+            throw new TooShortStringException();
+        }
+    }
+
+    public static class TooShortStringException extends Exception {
+    }
+
+    public static void main(String[] args) throws TooShortStringException {
+        System.out.println(getPartOfString("JavaRush\t - лучший сервис обучения Java."));
+    }
+}
