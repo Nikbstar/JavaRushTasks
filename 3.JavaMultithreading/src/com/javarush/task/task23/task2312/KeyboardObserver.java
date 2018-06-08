6a382429852b2c368b73f85d818b45dc46273376
Nikolay Kotkin <nikbstar@gmail.com>
2018-06-08 22:03:57 +0500

    Java Multithreading 3 уровень, 1 лекция

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/KeyboardObserver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/KeyboardObserver.java
@@ -2,16 +2,15 @@ package com.javarush.task.task23.task2312;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-
 public class KeyboardObserver extends Thread {
-    private Queue<KeyEvent> keyEvents = new ArrayBlockingQueue<>(100);
+    private Queue<KeyEvent> keyEvents = new ArrayBlockingQueue<KeyEvent>(100);
 
     private JFrame frame;
 
@@ -43,6 +42,7 @@ public class KeyboardObserver extends Thread {
 
 
         frame.addKeyListener(new KeyListener() {
+
             public void keyTyped(KeyEvent e) {
                 //do nothing
             }
--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
@@ -1,20 +1,19 @@
 package com.javarush.task.task23.task2312;
 
 public class Mouse {
-
     private int x;
     private int y;
 
-    public Mouse(final int x, final int y) {
+    public Mouse(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
     public int getX() {
-        return this.x;
+        return x;
     }
 
     public int getY() {
-        return this.y;
+        return y;
     }
 }
