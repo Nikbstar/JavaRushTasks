d0a7647965fdccdb0752e25f949f4e99b80a4a28
Nikolay Kotkin <nikbstar@gmail.com>
2018-06-27 11:01:30 +0500

    Java Multithreading 6 уровень, 1 лекция

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
@@ -4,11 +4,9 @@ public class User {
     private String name;
     private String surname;
     private int age;
+    private boolean man;
 
-    private String country;
-    private String city;
-    private House house;
-
+    private Address address;
     private Work work;
 
     public User(String name, String surname, int age) {
@@ -41,24 +39,32 @@ public class User {
         this.age = age;
     }
 
+    public boolean isMan() {
+        return man;
+    }
+
+    public void setMan(boolean man) {
+        this.man = man;
+    }
+
     public String getCountry() {
-        return country;
+        return this.address.getCountry();
     }
 
     public void setCountry(String country) {
-        this.country = country;
+        this.address.setCountry(country);
     }
 
     public String getCity() {
-        return city;
+        return this.address.getCity();
     }
 
     public void setCity(String city) {
-        this.city = city;
+        this.address.setCity(city);
     }
 
     public String getAddress() {
-        return country + " " + city + " " + house.house;
+        return this.address.getCountry() + " " + this.address.getCity() + " " + this.address.getHouse();
     }
 
     public Work getWork() {
@@ -68,4 +74,21 @@ public class User {
     public void setWork(Work work) {
         this.work = work;
     }
+
+    public void printInfo() {
+        System.out.println("Имя: " + this.name);
+        System.out.println("Фамилия: " + this.surname);
+    }
+
+    public void printAdditionalInfo() {
+        if (this.age < 16)
+            System.out.println("Пользователь моложе 16 лет");
+        else
+            System.out.println("Пользователь старше 16 лет");
+    }
+
+    public String getBoss() {
+        return this.work.getBoss();
+    }
+
 }
