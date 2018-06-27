d4c7e45e045018e276b11b2c255e2f351760da0f
Nikolay Kotkin <nikbstar@gmail.com>
2018-06-27 16:29:00 +0500

    Java Multithreading 6 уровень, 2 лекция (Был бы ум - будет и успех)

--- 3.JavaMultithreading/src/com/javarush/task/task26/task2601/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2601/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task26.task2601;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 /*
 Почитать в инете про медиану выборки
@@ -8,17 +9,20 @@ import java.util.Arrays;
 public class Solution {
 
     public static void main(String[] args) {
-        Integer[] array1 = {13,8,15,5,17};
-        System.out.println(Arrays.toString(sort(array1)));
-
-        Integer[] array2 = {8,15,5,17};
-        System.out.println(Arrays.toString(sort(array2)));
     }
 
     public static Integer[] sort(Integer[] array) {
         //implement logic here
         int mediana = getMediana(array);
-
+        Arrays.sort(array, (o1, o2) -> {
+            int m1 = Math.abs(mediana - o1);
+            int m2 = Math.abs(mediana - o2);
+            int result = m1 - m2;
+            if (result == 0) {
+                result = o1 - o2;
+            }
+            return result;
+        });
         return array;
     }
 
