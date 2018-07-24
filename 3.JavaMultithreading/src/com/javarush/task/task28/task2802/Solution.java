8d31aeb45f20cab182237b38669835965bb81e6e
Nikolay Kotkin <nikbstar@gmail.com>
2018-07-24 22:40:54 +0500

    Java Multithreading 8 уровень, 5 лекция

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2802/Solution.java
@@ -0,0 +1,67 @@
+package com.javarush.task.task28.task2802;
+
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/* 
+Пишем свою ThreadFactory
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+        class EmulateThreadFactoryTask implements Runnable {
+            @Override
+            public void run() {
+                emulateThreadFactory();
+            }
+        }
+
+        ThreadGroup group = new ThreadGroup("firstGroup");
+        Thread thread = new Thread(group, new EmulateThreadFactoryTask());
+
+        ThreadGroup group2 = new ThreadGroup("secondGroup");
+        Thread thread2 = new Thread(group2, new EmulateThreadFactoryTask());
+
+        thread.start();
+        thread2.start();
+    }
+
+    private static void emulateThreadFactory() {
+        AmigoThreadFactory factory = new AmigoThreadFactory();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        };
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+    }
+
+    public static class AmigoThreadFactory implements ThreadFactory {
+
+        private static AtomicInteger factoryCounter = new AtomicInteger(0);
+        private AtomicInteger poolCount = new AtomicInteger(0);
+        private AtomicInteger threadCount = new AtomicInteger(0);
+
+        {
+            poolCount.set(factoryCounter.incrementAndGet());
+        }
+
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread thread = new Thread(r);
+            thread.setDaemon(false);
+            thread.setPriority(Thread.NORM_PRIORITY);
+            String name = String.format("%s-pool-%d-thread-%d",
+                    Thread.currentThread().getThreadGroup().getName(),
+                    poolCount.get(),
+                    threadCount.incrementAndGet());
+            thread.setName(name);
+            return thread;
+        }
+    }
+
+}
